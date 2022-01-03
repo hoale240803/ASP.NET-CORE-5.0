1c718116860062a90e8df15ebf19460bdb994bb7
hoale240803 <hoale240803@gmail.com>
2022-01-03 08:44:11 +0700

    test restore auth

--- ASP.NETCORE5.0/ASP.NETCORE5.0/Controllers/WeatherForecastController.cs
+++ ASP.NETCORE5.0/ASP.NETCORE5.0/Controllers/WeatherForecastController.cs
@@ -76,5 +76,19 @@ namespace ASP.NETCORE5._0.Controllers
         }
 
 
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get4()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+
     }
 }
