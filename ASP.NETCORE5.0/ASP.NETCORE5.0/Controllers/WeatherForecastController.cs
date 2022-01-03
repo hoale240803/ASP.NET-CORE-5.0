a93deef2a9c5fc7b603227c2aa9b73cb3692414b
hoa.le <hoa.le@enlabsoftware.com>
2022-01-03 08:30:11 +0700

    test change credential

--- ASP.NETCORE5.0/ASP.NETCORE5.0/Controllers/WeatherForecastController.cs
+++ ASP.NETCORE5.0/ASP.NETCORE5.0/Controllers/WeatherForecastController.cs
@@ -35,5 +35,20 @@ namespace ASP.NETCORE5._0.Controllers
             })
             .ToArray();
         }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get1()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+
     }
 }
